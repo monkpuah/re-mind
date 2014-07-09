42fc06eb9d8fef28a110d9ed61ffaa231a0708fc
hani <farah.hani@zalora.com.my>
2014-07-09 23:00:22 +0800

    Created a new branch DogeMinder for the app. You will need 'gem install sqlite3 datamapper dm-sqlite-adapter' for this to run. The app now takes reminders(more like notes at this point) and display them.

---
+++ re-mind.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper'
+
+set :bind, '0.0.0.0'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/remind.db")
+
+class Reminder
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :due_date, DateTime
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
+
+get '/' do
+  @reminders = Reminder.all 
+  @title = 'Much Reminders'
+  erb :homepage
+end
+
+post '/' do
+	r = Reminder.new
+	r.content = params[:content]
+	r.created_at = Time.now
+	r.updated_at = Time.now
+	r.save
+	redirect '/'
+end
